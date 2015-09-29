9cdd504314afc05883f82901464b1138bfda8ba0
satyaAus <sathyavathi14@gmail.com>
2015-09-29 13:14:24 +1000

    sep29

---
+++ 29sep2015/src/com/sep29/classes/HelloJava.java
@@ -0,0 +1,17 @@
+package com.sep29.classes;
+
+/**
+ * 
+ * @author satyavathi
+ *
+ */
+public class HelloJava {
+
+	public static void sayHello(){
+		System.out.println("hello java");
+	}
+	
+	public void sayHello(String name){
+		System.out.printf("hello %s", name);	
+	}
+}
---
+++ 29sep2015/src/com/sep29/main/Sep29Main.java
@@ -0,0 +1,14 @@
+package com.sep29.main;
+
+import com.sep29.classes.HelloJava;
+
+public class Sep29Main {
+
+	public static void main(String[] args) {
+		HelloJava helloJavaObject = new HelloJava();
+		HelloJava.sayHello();
+		helloJavaObject.sayHello("babbe");
+		
+	}
+
+}
